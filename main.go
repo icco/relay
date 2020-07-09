34a24c57a301e88fd8ae26512b6dddcc17d842f3
Nat Welch <nat@natwelch.com>
2020-07-09 13:53:44 +0000

    add ping/pong

--- main.go
+++ main.go
@@ -69,6 +69,7 @@ func main() {
 	if err != nil {
 		log.WithError(err).Fatal("error creating Discord session")
 	}
+	dg.AddHandler(messageRecieve)
 
 	if err := dg.Open(); err != nil {
 		log.WithError(err).Fatal("error opening connection")
@@ -199,4 +200,24 @@ func fetchPrimaryTextChannelID(sess *discordgo.Session) (string, error) {
 	}
 
 	return channelid, nil
+}
+
+// messageRecieve will be called (due to AddHandler above) every time a new
+// message is created on any channel that the authenticated bot has access to.
+func messageRecieve(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	// If the message is "ping" reply with "Pong!"
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
+
+	// If the message is "pong" reply with "Ping!"
+	if m.Content == "pong" {
+		s.ChannelMessageSend(m.ChannelID, "Ping!")
+	}
 }
