bbfa591b641c888c5a8781689fb5f5d2829bcf0b
Nat Welch <nat@natwelch.com>
2020-07-20 15:13:17 +0000

    add a comment

--- lib/lock.go
+++ lib/lock.go
@@ -7,6 +7,7 @@ import (
 	"cirello.io/pglock"
 )
 
+// GetLockClient returns a lock client with default settings.
 func GetLockClient(db *sql.DB) (*pglock.Client, error) {
 	return pglock.New(db,
 		pglock.WithLeaseDuration(3*time.Second),
