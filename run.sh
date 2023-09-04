86835e324a38ab5fb0ef67c9d83be2285062d5a0
Nat Welch <nat@natwelch.com>
2023-09-04 21:53:13 +0000

    string

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ set -ex
 
 export PRANA_DB_URL=${DATABASE_URL}
 go run github.com/phogolabs/prana/cmd/prana -- migration run
+go run github.com/phogolabs/prana/cmd/prana -- model sync
 
 if [ ! -f $GOPATH/bin/relay ]; then
   go build -o $GOPATH/bin/relay .
